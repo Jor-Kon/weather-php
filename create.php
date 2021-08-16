02db200986c926b0c617dc34fa6ff69b0c71cd5d
Jordan Kondracki <jordankondracki1999@gmail.com>
2021-08-16 16:47:03 +0200

    simple sign up functionality

---
+++ create.php
@@ -0,0 +1,114 @@
+<?php
+require_once "config.php";
+ 
+$firstname = "";
+$lastname = "";
+$email = "";
+$password = "";
+
+$firstname_err = "";
+$lastname_err = "";
+$email_err = "";
+$password_err = "";
+
+
+ 
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $input_firstname = trim($_POST["firstname"]);
+    if(empty($input_firstname)){
+        $name_err = "Please enter firstname.";
+    } else{
+        $firstname = $input_firstname;
+    }
+
+    $input_lastname = trim($_POST["lastname"]);
+    if(empty($input_lastname)){
+        $name_err = "Please enter lastname.";
+    } else{
+        $lastname = $input_lastname;
+    }
+    
+    $input_email = trim($_POST["email"]);
+    if(empty($input_email)){
+        $address_err = "Please enter email.";     
+    } else{
+        $email = $input_email;
+    }
+    
+    $input_password = trim($_POST["password"]);
+    if(empty($input_password)){
+        $salary_err = "Please enter password.";     
+    } else{
+        $password = $input_password;
+    }
+    
+    if(empty($firstname_err) && empty($lastname_err) && empty($email_err) && empty($password_err)){
+        $sql = "INSERT INTO users (firstname, lastname, email, password) VALUES (?, ?, ?, ?)";
+         
+        if($stmt = mysqli_prepare($link, $sql)){
+            mysqli_stmt_bind_param($stmt, "ssss", $param_firstname, $param_lastname, $param_email, $param_password);
+            
+            $param_firstname = $firstname;
+            $param_lastname = $lastname;
+            $param_email = $email;
+            $param_password = $password;
+            
+            if(mysqli_stmt_execute($stmt)){
+                header("location: index.php");
+                exit();
+            } else{
+                echo "ERROR";
+            }
+        }
+        mysqli_stmt_close($stmt);
+    }
+    mysqli_close($link);
+}
+?>
+ 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Create Record</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
+</head>
+<body>
+    <div class="mx-auto" style="width: 600px;">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <h2 class="mt-5">Sign up</h2>
+                    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+                        <div class="form-group">
+                            <label>First Name</label>
+                            <input type="text" name="firstname" class="form-control <?php echo (!empty($name_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $firstname; ?>">
+                            <span class="invalid-feedback"><?php echo $name_err;?></span>
+                        </div>
+                        <div class="form-group">
+                            <label>Last Name</label>
+                            <input type="text" name="lastname" class="form-control <?php echo (!empty($name_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $lastname; ?>">
+                            <span class="invalid-feedback"><?php echo $name_err;?></span>
+                        </div>
+                        <div class="form-group">
+                            <label>Email</label>
+                            <input type="text" name="email" class="form-control <?php echo (!empty($name_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $email; ?>">
+                            <span class="invalid-feedback"><?php echo $name_err;?></span>
+                        </div>
+                        <div class="form-group">
+                            <label>Password</label>
+                            <input type="password" name="password" class="form-control <?php echo (!empty($name_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $password; ?>">
+                            <span class="invalid-feedback"><?php echo $name_err;?></span>
+                        </div>
+                        <div class="mt-3">
+                            <input type="submit" class="btn btn-primary" value="Submit">
+                            <a href="index.php" class="btn btn-secondary ml-2">Cancel</a>
+                        </div>
+                    </form>
+                </div>
+            </div>        
+        </div>
+    </div>
+</body>
+</html>
